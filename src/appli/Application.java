704bfc68dabe2285faeae1d515510a1b50fce708
Marina Blin <marina.blin@free.fr>
2015-12-14 17:19:22 +0100

    Ajout JavaDoc pour la classe Application

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -1,12 +1,28 @@
+/**
+ * Projet Programmation 2 - Licence professionnel SIL en alternance
+ * 14 décembre 2015 / 15 janvier 2016
+ *  
+ * @author Cadorel Maël - Blin Marina
+ * @version 1.0.0
+ */
+
+/**
+ * Localisation de la classe
+ */
 package appli;
 
+/**
+ * Classe principal de l'application
+ */
 public class Application
 {
-
+	/**
+	 * Méthode principal permettant de lancer l'application
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 
 	}
-
 }
